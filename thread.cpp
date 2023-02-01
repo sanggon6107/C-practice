7e9c7bed4d6cfb0b55460012cbe40d43f79c0379
sanggon6107 <sanggon6107@naver.com>
2023-02-01 21:54:22 +0900

    thread의 다양한 생성 방법

--- thread.cpp
+++ thread.cpp
@@ -1,22 +1,38 @@
 #include <iostream>
 #include <chrono>
 #include <thread>
+#include <functional>
+
 using namespace std;
 
-void function()
+class Class
+{
+public :
+	void MemberFunc() {}
+};
+
+
+
+void Function1()
 {
 	cout << "function" << endl;
 	this_thread::sleep_for(5s);
 }
 
-void function_2(int& a, int b)
+void Function2(int& a, int b)
 {
 	a = 30;
 }
 
+void Function3(int a, int b)
+{
+	cout << "function_3" << endl;
+}
+
+
 int main()
 {
-	thread th1(&function);
+	thread th1(&Function1);
 
 	// 아래의 둘 중 하나의 함수는 호출해야 에러 뜨지 않음.
 	th1.join(); // 이 줄에서 th1이 끝날 때 까지 대기
@@ -24,10 +40,18 @@ int main()
 
 	int arg_1 = 1;
 	int arg_2 = 2;
-	thread th2(&function_2, ref(arg_1), arg_2); // 스레드 실행법 1 : thread의 객체를 생성하면서, 함수의 주소와 인자를 thread의 생성자에 전달하면 된다. reference 타입은 ref()를 사용.
-
-
-
-	th2.join();
+	thread th2(&Function2, ref(arg_1), arg_2); // 스레드 실행법 1 : thread의 객체를 생성하면서, 함수의 주소와 인자를 thread의 생성자에 전달하면 된다. reference 타입은 ref()를 사용.
 	
+	// 객체의 멤버 함수
+	Class c_1();
+	thread th3(&Class::MemberFunc);
+
+	// 람다식(캡쳐 등등은 어떻게 동작하는가?)
+	thread th4([](int a, int b) {cout << a + b << endl; }, 1, 2);
+
+	thread th2(bind(&Function3, 1, 2) );
+
+	if (th2.joinable()) { th2.join(); }
+	if (th3.joinable()) { th3.join(); }
+	if (th4.joinable()) { th4.join(); }
 }
